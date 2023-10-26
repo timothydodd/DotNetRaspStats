bd1d6c0266dfa17b01726a22898d83557ba9dbfd
Tim Dodd <tdodd@me.com>
2023-10-26 16:27:11 -0400

    Fixed Issue with Screen not clearing

--- DotNetRaspStats/Worker.cs
+++ DotNetRaspStats/Worker.cs
@@ -1,85 +1,80 @@
+using System.Diagnostics;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Sang.IoT.SSD1306;
 using SkiaSharp;
-public class Worker : BackgroundService
+public class Worker : BackgroundService, IDisposable
 {
     private readonly ILogger<Worker> _logger;
     private readonly IConfiguration _configuration;
-
+    private readonly SSD1306_128_64 display;
+    private SKPaint paint;
     public Worker(ILogger<Worker> logger, IConfiguration configuration)
     {
         _logger = logger;
         _configuration = configuration;
+        display = new SSD1306_128_64(1);
+        var currentFolder = Path.GetDirectoryName(Process.GetCurrentProcess().MainModule?.FileName);
+        if (currentFolder == null)
+            throw new Exception("Could not find current folder");
+        paint = new()
+        {
+            Color = new SKColor(255, 255, 255),
+            StrokeWidth = 1,
+            Typeface = SKTypeface.FromFile(Path.Combine(currentFolder, "assets/RobotoRegular-3m4L.ttf")),
+            TextSize = 13,
+            Style = SKPaintStyle.Fill,
+        };
     }
-    // public override async Task StopAsync(CancellationToken stoppingToken)
-    // {
-    //     _logger.LogInformation(
-    //         "Consume Scoped Service Hosted Service is stopping.");
-
-    //     await base.StopAsync(stoppingToken);
-    // }
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
         _logger.LogInformation("Starting Up");
 
         Graph g = new Graph(17);
-        using (var oled = new SSD1306_128_64(1))
+
+        ScreenStack stack = new();
+
+        var ip = Metrics.GetLocalIPAddress();
+
+        while (stoppingToken.IsCancellationRequested == false)
         {
-            var currentFolder = Path.GetDirectoryName(System.Diagnostics.Process.GetCurrentProcess().MainModule.FileName);
-            Console.WriteLine(currentFolder);
-            SKPaint paint = new()
-            {
-                Color = new SKColor(255, 255, 255),
-                StrokeWidth = 1,
-                Typeface = SKTypeface.FromFile(Path.Combine(currentFolder, "assets/RobotoRegular-3m4L.ttf")),
-                TextSize = 13,
-                Style = SKPaintStyle.Fill,
-            };
-            ScreenStack stack = new();
-
-            var ip = Metrics.GetLocalIPAddress();
-
-            while (stoppingToken.IsCancellationRequested == false)
-            {
-                MemoryMetrics metrics = Metrics.GetUnixMetrics();
-                UptimeMetrics cpuUsage = Metrics.GetCpuMetrics();
-                var totalMemory = Metrics.SizeSuffix((long)metrics.Total, 1);
-                var usedMemory = Metrics.SizeSuffix((long)metrics.Used, 1);
-                g.Update((byte)cpuUsage.CPU);
-                stack.Add($"IP:{ip}");
-                stack.Add($"CPU: {cpuUsage.CPU}%");
-                stack.AddGraph(g);
-                stack.Add($"Mem: {usedMemory} / {totalMemory}");
-                stack.Add($"Up: {cpuUsage.UpTime}");
+            MemoryMetrics metrics = Metrics.GetUnixMetrics();
+            UptimeMetrics cpuUsage = Metrics.GetCpuMetrics();
+            var totalMemory = Metrics.SizeSuffix((long)metrics.Total, 1);
+            var usedMemory = Metrics.SizeSuffix((long)metrics.Used, 1);
+            g.Update((byte)cpuUsage.CPU);
+            stack.Add($"IP:{ip}");
+            stack.Add($"CPU: {cpuUsage.CPU}%");
+            stack.AddGraph(g);
+            stack.Add($"Mem: {usedMemory} / {totalMemory}");
+            stack.Add($"Up: {cpuUsage.UpTime}");
 
 
-                oled.Begin();
-                oled.Clear();
+            display.Begin();
+            display.Clear();
 
-                using SKBitmap bitmap = new(128, 64, true);
-
-                using SKCanvas canvas = new(bitmap);
-
-                stack.Draw(canvas, paint);
-
-                oled.Image(bitmap.Encode(SKEncodedImageFormat.Png, 100).ToArray());
+            using SKBitmap bitmap = new(128, 64, true);
+            using SKCanvas canvas = new(bitmap);
+            stack.Draw(canvas, paint);
+            display.Image(bitmap.Encode(SKEncodedImageFormat.Png, 100).ToArray());
 
 
-                oled.Display();
-                stack.Reset();
-                if (stoppingToken.IsCancellationRequested)
-                    break;
-                await Task.Delay(2000, stoppingToken);
-                // stoppingToken.WaitHandle.WaitOne(5000);
-            }
-
-            // Clear Screen on exit
-            Console.WriteLine("Exiting");
-            oled.Begin();
-            oled.Clear();
-            oled.Display();
+            display.Display();
+            stack.Reset();
+            if (stoppingToken.IsCancellationRequested)
+                break;
+            await Task.Delay(5000, stoppingToken);
         }
+
+    }
+    override public void Dispose()
+    {
+        Console.WriteLine("Clearing Screen");
+        display.Begin();
+        display.Clear();
+        display.Display();
+        display.Dispose();
+        base.Dispose();
     }
 }
